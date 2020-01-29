842233ac3c5fecac7d8e404029bed85923d6b330
Andrei_Viarenich <aviarenich@copyright.com>
2020-01-29 15:38:00 +0300

    refactoring

--- src/main/java/com/andver/firstApp/repository/PersonRepository.java
+++ src/main/java/com/andver/firstApp/repository/PersonRepository.java
@@ -2,9 +2,9 @@ package com.andver.firstApp.repository;
 
 import com.andver.firstApp.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RestResource(path = "personsRestResource")
+@RepositoryRestResource(path = "personsRestResource")
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     Iterable<Person> findByName(String name);
